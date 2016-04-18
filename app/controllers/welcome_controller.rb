7d2dae561144f54053a503a7d6a34f7764af2841
emiliano <chequeremiliano@gmail.com>
2016-04-18 12:39:28 -0300

    Commit inicial

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,32 @@
 class WelcomeController < ApplicationController
+	 before_action :load_data, only: [:index, :brand, :category]
+
   def index
-  @latest_products = Product.order('created_at desc').limit(4)
-    @sale_products = Product.where(sale: true).order('created_at desc').limit(6)
-  end  
+    @products = Product.all
+  end
+
+  def brand
+    @products = Product.includes(:brands).where(brands: {id:params[:brand_id]})
+    render 'index'
+  end
+
+  def category
+    @products = Product.includes(:categories).where(categories: {id: params[:category_id]})
+    render 'index'
+  end
+
+ # def index
+  	
+  #@latest_products = Product.order('created_at desc').limit(4)
+   #@sale_products = Product.where(sale: true).order('created_at desc').limit(6)
+  #end  
+
+
+
+  protected
+
+  def load_data
+    @brands = Brand.order(:name).all
+    @categories = Category.order(:name).all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+ get 'marca/:brand_id' => 'welcome#brand', as: :home_brand
+  get 'categoria/:category_id' => 'welcome#category', as: :home_category
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
